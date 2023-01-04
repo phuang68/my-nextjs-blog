04d40f16aa635ab003f6ee11875a21be8731860f
bryan <phuang68@uwo.ca>
2023-01-04 22:22:25 +0800

    [bryan] feat: add first post component

---
+++ pages/posts/first-post/index.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+const FirstPost = () => {
+  return <>
+    <h1>First Post</h1>
+    <h2><Link href={'/'} >Back to Home</Link></h2>
+  </>
+}
+
+export default FirstPost;
