879fad316305e66e432a0d0564864d3dde78a356
bryan <phuang68@uwo.ca>
2023-01-05 16:44:15 +0800

    [bryan] feat: add link to first post page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
@@ -7,11 +8,13 @@ export default function Home() {
         <title>Bryan Huang Blog</title>
         <meta name="description" content="Bryan Huang's Blog by Next.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icons/favicon.ico" />
       </Head>
       <main className={styles["main"]}>
-        Hello World
+        <h1 className={styles["title"]}>
+          Read <Link href={"/posts/first-post"}>this page!</Link>
+        </h1>
       </main>
     </>
-  )
+  );
 }
